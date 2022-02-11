56bf853e74cc9b695d9894e6a8aaffb559a03268
breanner06 <92890666+breanner06@users.noreply.github.com>
2022-02-10 18:30:49 -0600

    update

--- assignment4.js
+++ assignment4.js
@@ -21,22 +21,46 @@ document.getElementById("word1").innerHTML = word1;
 document.getElementById("word2").innerHTML = word2;
 
 
-
 function addWords () {
-    document.getElementById("compoundWord").innerHTML =
-    text.replace ("click here for compound word", function (x) {
-        return compoundWord});
+    document.getElementById("compoundWord").innerHTML = compoundWord;
 }
 
 function upperCase () {
     document.getElementById("upperCase").innerHTML = 
-    caps.replace ("click here for upper case", compoundWord.toUpperCase ());
+    caps.replace (caps, compoundWord.toUpperCase());
 }
 
 function lowerCase () {
     document.getElementById("lowerCase").innerHTML = 
-    lowers.replace ("click here for lower case", compoundWord.toLowerCase ());
+    lowers.replace (lowers, compoundWord.toLowerCase ());
 }
+
+let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case  6:
+    day = "Saturday";
+}
+let str = document.getElementById("today").innerHTML; 
+let res = str.replace(/today/g, day);
+document.getElementById("today").innerHTML = res;
 /*
 create a concatenated string variable that includes other
 embedded variables concatenated together. output to page or consol
